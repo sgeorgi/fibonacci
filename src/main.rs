cdd6a3d21bfd330edd91ed0d5454ce0ed924b743
Sebastian Georgi <sebastian@georgi.io>
2021-08-18 08:31:26 +0200

    Implemented a recursive Fibonacci calculator

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn fibonacci(x: u16) -> u16 {
+    match x {
+        0 => 0,
+        1 | 2 => 1,
+        _ => fibonacci(x - 1) + fibonacci(x - 2)
+    }
+}
+
+
+fn main() {
+    println!("Welcome to the Fibonacci Calculator");
+    println!("\nPlease specify the n in Fn!");
+
+    let mut character: String = String::new();
+
+    io::stdin()
+        .read_line(&mut character)
+        .expect("Failed to read line");
+
+    let number: u16 = match character.trim().parse() {
+        Ok(num) => { num }
+        Err(_) => {
+            println!("No number given, bailing out!");
+            panic!()
+        }
+    };
+
+    println!("Calculating the {}nth Fibonacci number ... ", number);
+    println!("{}", fibonacci(number))
+}
